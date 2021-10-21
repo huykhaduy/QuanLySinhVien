e6a58bed55957bca7b97c7ab4806ed3f55c99ba9
Duy <huykhaduy@gmail.com>
2021-10-22 00:17:00 +0700

    Update file

---
+++ Main.java
@@ -0,0 +1,220 @@
+import SinhVien.*;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    static LinkedList<sinhvien> listSV = new LinkedList<sinhvien>();
+    // Variable using for check student exist or not 
+    static boolean isHasStudent = false; 
+
+    public static void main(String argv[]) {
+        int choice = 0;
+        boolean isLoop = true;
+        while (isLoop) {
+            System.out.println("-----------------MENU QUAN LY SINH VIEN-----------------");
+            System.out.println("1. Them sinh vien");
+            System.out.println("2. Xoa sinh vien");
+            System.out.println("3. Sua sinh vien");
+            System.out.println("4. Nhap diem cho sinh vien");
+            System.out.println("5. Xem thong tin va diem cua sinh vien");
+            System.out.println("6. Xuat ra danh dach sinh vien");
+            System.out.println("7. Tim kiem sinh vien co diem theo dieu kien (dang phat trien)");
+            System.out.println("8. In ra danh sach sinh vien (dang phat trien)");
+            System.out.println("9. Thoat");
+            System.out.print("> Lua chon cua ban: ");
+            choice = sc.nextInt();
+            sc.nextLine();
+            boolean isExist = false;
+            switch (choice) {
+            case 1: //Them sinh vien
+                listSV.add(createStudent());
+                break;
+            case 2: // Xoa sinh vien
+                isExist = removeStudent();
+                checkExistStudent(isExist);
+                break;
+            case 3: // Thay doi thong tin
+                isExist = changeSinhVien();
+                checkExistStudent(isExist);
+                break;
+            case 4: // Them diem
+                isExist = addPointSinhVien();
+                checkExistStudent(isExist);
+                break;
+            case 5: // code
+                isExist = showStudentInfo();
+                checkExistStudent(isExist);
+                break;
+            case 6: // code
+                showListOfStudent();
+                break;
+            case 7: // code
+                inUpdate();
+                break;
+            case 8: //code 
+                inUpdate();
+                break;
+            case 9: //code 
+                isLoop = false;
+                break;
+            default://code
+                System.out.println("Lua chon cua ban khong hop le! Vui long chon lai");
+                break;
+            }
+        }
+        sc.close();
+    }
+
+    public static sinhvien createStudent() {
+        System.out.print("+ Nhap ho va ten sinh vien: ");
+        String name = sc.nextLine();
+        System.out.print("+ Nhap so dien thoai: ");
+        String phone = sc.nextLine();
+        System.out.print("+ Nhap lop: ");
+        String lop = sc.nextLine();
+        System.out.print("+ Nhap khoa: ");
+        String khoa = sc.nextLine();
+        sinhvien sv = new sinhvien(name, phone, lop, khoa);
+        System.out.println("Them sinh vien thanh cong!");
+        return sv;
+    }
+
+    public static boolean removeStudent() {
+        System.out.print("> Nhap MSSV muon xoa: ");
+        int pos = sc.nextInt();
+        if (pos < 1 || pos > sinhvien.getTotal()) {
+            return false;
+        }
+        listSV.forEach(item -> {
+            if (item.getMssv() == pos) {
+                isHasStudent = true;
+                System.out.println(
+                        "Ban co muon xoa sinh vien " + item.getName() + " (mssv: " + item.getMssv() + ") khong ?");
+                System.out.println("1. Co");
+                System.out.println("2. Khong");
+                System.out.print("> Lua chon cua ban: ");
+                int confirm = sc.nextInt();
+                sc.nextLine();
+                if (confirm == 1) {
+                    if (listSV.remove(item) == true) {
+                        System.out.println("Xoa thanh cong " + item.getName() + " !");
+                    }
+                } else {
+                    System.out.println("Huy xoa thanh cong!");
+                }
+            }
+        });
+        if (isHasStudent) {
+            isHasStudent = false;
+            return true;
+        }
+        return false;
+    }
+    
+    public static boolean changeSinhVien() {
+        System.out.print("> Nhap mssv muon thay doi: ");
+        int pos = sc.nextInt();
+
+        if (pos < 1 || pos > sinhvien.getTotal()) {
+            return false;
+        }
+        listSV.forEach(item -> {
+            if (item.getMssv() == pos) {
+                isHasStudent = true;
+                System.out.println("Ban co muon thay doi sinh vien " + item.getName() + " (mssv: " + item.getMssv()
+                        + " ) khong ?");
+                System.out.println("1. Co");
+                System.out.println("2. Khong");
+                System.out.print("> Lua chon cua ban: ");
+                int confirm = sc.nextInt();
+                sc.nextLine();
+                if (confirm == 1) {
+                    System.out.print("+ Nhap ho va ten sinh vien: ");
+                    String name = sc.nextLine();
+                    System.out.print("+ Nhap so dien thoai: ");
+                    String phone = sc.nextLine();
+                    System.out.print("+ Nhap lop: ");
+                    String lop = sc.nextLine();
+                    System.out.print("+ Nhap khoa: ");
+                    String khoa = sc.nextLine();
+                    item.setInfo(name, phone, lop, khoa);
+                    System.out.println("Thay doi thong tin sinh vien thanh cong!");
+                } else {
+                    System.out.println("Huy thay doi thanh cong!");
+                }
+            }
+        });
+        if (isHasStudent) {
+            isHasStudent = false;
+            return true;
+        }
+        return false;
+    }
+    
+    public static boolean addPointSinhVien() {
+        System.out.print("> Nhap mssv muon them diem: ");
+        int pos = sc.nextInt();
+        sc.nextLine();
+        if (pos < 1 || pos > sinhvien.getTotal()) {
+            return false;
+        }
+        listSV.forEach(item -> {
+            if (item.getMssv() == pos) {
+                isHasStudent = true;
+                System.out.println("Sinh vien: " + item.getName() + " (mssv: " + item.getMssv() + ")");
+                System.out.print("+ Nhap diem toan: ");
+                int diemToan = sc.nextInt();
+                System.out.print("+ Nhap diem hoa: ");
+                int diemHoa = sc.nextInt();
+                System.out.print("+ Nhap diem ly: ");
+                int diemLy = sc.nextInt();
+                sc.nextLine();
+                item.setPoint(diemToan, diemHoa, diemLy);
+                System.out.println("Them diem thanh cong!");
+            }
+        });
+        if (isHasStudent) {
+            isHasStudent = false;
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean showStudentInfo() {
+        System.out.print("> Nhap mssv de xem thong tin: ");
+        int pos = sc.nextInt();
+        sc.nextLine();
+        if (pos < 1 || pos > sinhvien.getTotal()) {
+            return false;
+        }
+        listSV.forEach(item -> {
+            if (item.getMssv() == pos) {
+                isHasStudent = true;
+                item.showInformation();
+            }
+        });
+        if (isHasStudent) {
+            isHasStudent = false;
+            return true;
+        }
+        return false;
+    }
+
+    public static void showListOfStudent() {
+        System.out.println("Danh sach sinh vien: ");
+        listSV.forEach(item ->{
+            item.showInList();
+        });
+    }
+
+    public static void checkExistStudent(boolean isHasStudent) {
+        if (!isHasStudent) {
+            System.out.println("Ma so sinh vien khong ton tai!");
+        }
+    }
+
+    public static void inUpdate() {
+        System.out.println("Chuc nang nay dang duoc phat trien!");
+    }
+}
---
+++ SinhVien/sinhvien.java
@@ -0,0 +1,170 @@
+package SinhVien;
+
+// import java.util.*;
+
+public class sinhvien {
+    private static int id = 0;
+    private int mssv;
+    private String name;
+    private String phone;
+    private String lop;
+    private String khoa;
+    private float diemToan;
+    private float diemHoa;
+    private float diemLy;
+    //Boolean for check is set diem 
+    private boolean isSetToan = false;
+    private boolean isSetLy = false;
+    private boolean isSetHoa = false;
+    private boolean isSet = false;
+
+    private void checkState() {
+        if (isSetToan && isSetLy && isSetHoa) {
+            isSet = true;
+        } else {
+            isSet = false;
+        }
+    }
+
+    public sinhvien() {
+        this.mssv = ++id;
+    }
+
+    public sinhvien(String name, String phone, String lop, String khoa, float diemToan, float diemHoa, float diemLy) {
+        this.mssv = ++id;
+        setInfo(name, phone, lop, khoa);
+        setPoint(diemToan, diemHoa, diemLy);
+    }
+
+    public sinhvien(String name, String phone, String lop, String khoa) {
+        this.mssv = ++id;
+        setInfo(name, phone, lop, khoa);
+    }
+
+
+    public static int getTotal() {
+        return sinhvien.id;
+    }
+
+    public int getMssv() {
+        return this.mssv;
+    }
+
+    public void setInfo(String name, String phone, String lop, String khoa) {
+        setName(name);
+        setPhone(phone);
+        setLop(lop);
+        setKhoa(khoa);
+    }
+
+    public void setPoint(float diemToan,float diemHoa,float diemLy){
+        setDiemToan(diemToan);
+        setDiemLy(diemLy);
+        setDiemHoa(diemHoa);
+    }
+
+    public void setName(String name) {
+        if (name != "")
+            this.name = name;
+        else this.name = "Chua co ten";
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setPhone(String phone) {
+        if (phone.length() >= 9 && phone.length() <= 11) {
+            this.phone = phone;
+        }
+        else this.phone = "So dien thoai khong hop le";
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setLop(String lop) {
+        if (lop.length() >= 2)
+            this.lop = lop;
+        else this.lop = "Trong";
+    }
+
+    public String getLop() {
+        return this.lop;
+    }
+
+    public void setKhoa(String khoa) {
+        if (khoa.length() >= 2)
+            this.khoa = khoa;
+        else this.khoa = "Trong";
+    }
+
+    public String getKhoa() {
+        return this.khoa;
+    }
+
+    public void setDiemToan(float diemToan) {
+        if (diemToan >= 0 && diemToan <= 10) {
+            this.diemToan = diemToan;
+            isSetToan = true;
+            checkState();
+        }     
+        else this.diemToan = 0;
+    }
+
+    public float getDiemToan() {
+        return this.diemToan;
+    }
+
+    public void setDiemLy(float diemLy) {
+        if (diemLy >= 0 && diemLy <= 10) {
+            this.diemLy = diemLy;
+            isSetLy = true;
+            checkState();
+        }
+        else this.diemLy = 0;
+    }
+
+    public float getDiemLy() {
+        return this.diemLy;
+    }
+
+    public void setDiemHoa(float diemHoa) {
+        if (diemHoa >= 0 && diemHoa <= 10) {
+            this.diemHoa = diemHoa;
+            isSetHoa = true;
+            checkState();
+        }    
+        else this.diemHoa = 0;
+    }
+
+    public float getDiemHoa() {
+        return this.diemHoa;
+    }
+
+    public float getDiemTrungBinh() {
+        return (this.diemHoa + this.diemToan + this.diemLy) / 3;
+    }
+
+    public void showInformation() {
+        System.out.println("MSSV: " + mssv);
+        System.out.println("Ho va ten: " + name);
+        System.out.println("Lop: " + lop);
+        System.out.println("Khoa: " + khoa);
+        if (isSet) {
+            System.out.println("Diem toan: " + diemToan);
+            System.out.println("Diem hoa: " + diemHoa);
+            System.out.println("Diem ly" + diemLy);
+            System.out.println("Diem trung binh cua sinh vien: " + getDiemTrungBinh());
+        } else {
+            System.out.println("Sinh vien chua duoc nhap diem!");
+        }
+
+    }
+    
+    public void showInList() {
+        System.out.println("MSSV: " + mssv +", name: " + name);
+    }
+}
